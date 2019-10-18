d58f2237a3749267623f9309f64ecf9a87891e27
Satyam Chand Deopa <53179013+SatyamChand@users.noreply.github.com>
2019-10-18 14:13:17 +0530

    Added comments

--- Codechef/Count Substrings.cpp
+++ Codechef/Count Substrings.cpp
@@ -1,6 +1,7 @@
 #include<bits/stdc++.h>
 #define ll long long int
 using namespace std;
+
 int main()
 {
 	ll t;
@@ -11,14 +12,14 @@ int main()
 		cin>>n;		
 		string s;
 		cin>>s;
-		for(int i=0;i<n;i++)
+		for(int i=0;i<n;i++)			//Counting the number of substrings
 		{
 			if(s[i]=='1')
 				c1++;
 		}
-		if(c1 == 0)
-            cout<<"0\n";
-        else
+		if(c1 == 0)				//If substrings=0
+            		cout<<"0\n";
+        	else					//Else
 			cout<<(c1*(c1+1)/2)<<"\n";
 	}
 	return 0;
